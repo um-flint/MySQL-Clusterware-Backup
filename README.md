4d5668203768c0bcb30a29bffdefd58f2415f8e2
Robinson, Adam <adarobin@umflint.edu>
2017-04-26 13:35:58 -0400

    initial import

---
+++ README.md
@@ -0,0 +1,22 @@
+This code acts as a wrapper for scheduling backup jobs for MySQL instances that are running on top of Oracle Clusterware with MySQL Enterprise Backup.
+
+By scheduling this as a cron job on every node in the cluster, you can make sure the databases are backed up from the currently active node only.
+
+Currently, this code assumes:
+ * You are using Oracle Secure Backup to write to tapes as your backup target.
+ * You have configured a local account to run this job that has permission to log in to the MySQL database using auth_socket
+ * You have a copy of MySQL Enterprise Backup located inside the MYSQL_HOME in a directory named 'meb'
+ * You are not using MySQL that was installed from a package (i.e. you are using the tar version)
+ * You are using Python 2.7
+
+If you are using a distribution that includes an older version of Python (such as Oracle Linux 6) you will need to install Python 2.7 from Software Collections.
+
+You need to copy the sample config to a file named mysqlClusterwareBackup.cfg and tweak it to your environment.  The config file needs to be in the same directory as the script.
+
+It has been tested with:
+ * Oracle Linux 6
+ * MySQL 5.6 and 5.7
+ * MySQL Enterprise Backup 3.12.3 and 4.1.0
+ * Oracle Grid Infrastructure/Clusterware 12.1.0.2
+ * Oracle Grid Infrastructure Standalone Agent 7.1.0
+
---
+++ mysqlClusterwareBackup.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python
+
+import ConfigParser
+import subprocess
+import socket
+import time
+import shutil
+
+def main():
+    config = ConfigParser.ConfigParser()
+    config.readfp(open('mysqlClusterwareBackup.cfg'))
+
+    xag_home = config.get('clusterware','xag_home')
+    grid_home = config.get('clusterware','grid_home')
+    instances = config.get('clusterware', 'instances')
+
+    agctl = xag_home + '/bin/agctl'
+    hostname = socket.gethostname().split('.')[0]
+
+    for instance in instances.split(','):
+        instance_name = instance.strip()
+        
+        xag_status = subprocess.check_output([agctl, 'status', 'mysql_server', instance_name]).strip()
+
+        if 'is running on' in xag_status:
+            if hostname in xag_status:
+                print 'Instance', instance_name, 'is running on this node - continuing'
+                instance_config = subprocess.check_output([agctl, 'config', 'mysql_server', instance_name]).strip()
+
+                for line in instance_config.split('\n'):
+                    if line.startswith('Mysql home:'):
+                        mysql_home = line.split(':')[1].strip()
+
+                    if line.startswith('Datadir:'):
+                        datadir = line.split(':')[1].strip()
+
+                if mysql_home is None:
+                    print '  Could not find MySQL Home for', instance_name, '- skipping it'
+
+                elif datadir is None:
+                    print '  Could not find MySQL Home for', instance_name, '- skipping it'
+
+                else:
+                    print '  Attempting to backup instance', instance_name
+
+                    mysql_socket = datadir + '/mysql.sock'
+
+                    use_osb = config.getboolean('mysqlbackup', 'use_osb')
+
+                    if use_osb is True:
+                        sbt_db = config.get('mysqlbackup', 'sbt-database-name')
+                        backup_dir = mysql_home + '/' + config.get('mysqlbackup', 'backup-dir')
+                        backup_user = config.get('mysqlbackup', 'user')
+                        verbose_output = config.getboolean('mysqlbackup', 'verbose')
+
+                        meb = mysql_home + '/meb/mysqlbackup'
+                        socket_arg = '--socket='+mysql_socket
+                        user_arg = '--user='+backup_user
+                        bi_arg = '--backup-image=sbt'+instance_name+time.strftime('%Y-%m-%d')
+                        sbt_db_arg = '--sbt-database-name='+sbt_db
+                        bdir_arg = '--backup-dir='+backup_dir
+                        btype_arg = 'backup-to-image'
+                        
+                        try: 
+                            backup_run = subprocess.check_output([meb, socket_arg, user_arg, bi_arg, sbt_db_arg, bdir_arg, btype_arg], stderr=subprocess.STDOUT).strip()
+                            print '  mysqlbackup return code was 0 - backup appears to have succeeded'
+                            if verbose_output is True:
+                                print '  verbose output enabled - output from mysqlbackup follows'
+                                print '**************************************************************'
+                                print backup_run
+                                print '**************************************************************'
+                            else:
+                                if backup_run.endswith('mysqlbackup completed OK!'):
+                                    print '  last line of output was "mysqlbackup completed OK!" - success!'
+                                else:
+                                    print '  unable to verify backup was successful - output from mysqlbackup follows'
+                                    print '**************************************************************'
+                                    print backup_run
+                                    print '**************************************************************'
+
+                        except subprocess.CalledProcessError as e:
+                            print '  mysqlbackup return code was', e.returncode, '- backup failed'
+                            print '  The output from mysqlbackup follows'
+                            print '**************************************************************'
+                            print e.output.strip()
+                            print '**************************************************************'
+
+                        print '  removing',  backup_dir
+                        shutil.rmtree(backup_dir)
+
+                    else:
+                        print '  Backups not using Oracle Secure Backup not implemented yet - skipping'
+
+            else:
+                print 'Instance', instance_name, 'is not running on this node - skipping it'
+        elif 'is not running' in xag_status:
+            print 'Instance', instance_name, 'is  not running at all - skipping it'
+
+        else:
+            print 'Instance', instance_name, 'appears to have an unusual status - skipping and displaying output from agctl'
+            print ' ', xag_status
+        print
+        
+    return
+
+if __name__ == '__main__':
+    main()
